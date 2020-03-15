05889c847be9d8a2d575c772a2e86a392cebe366
Anubhav Garg <anubhav@tripeur.com>
2020-03-15 23:37:03 +0530

    changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ limit: '1mb' }));
 app.use("/api/v1/file", require("./routes/file"));
 
 var server = app.listen(8000, () => {
-    logger.log('info', `App is running in port 8000`, 'App default port')
+    console.log('info', `App is running in port 8000`, 'App default port')
 });
 server.setTimeout(500000);
 
--- routes/file.js
+++ routes/file.js
@@ -4,11 +4,18 @@ var router = require("express").Router();
 const upload = multer({ dest: "temp/" });
 const path = require("path");
 
-router.post("/", upload.single("upload"), (req, res) => {
+router.post("/", upload.single("file"), async(req, res) => {
     var fileInfo = req.file;
-    // var ext = path.extname(fileInfo.originalname);
     var filepath = fileInfo.path;
-    console.log(filepath, "assasad")
+    console.log(`the file information is `, filepath)
+    console.log("Here is the output")
+    await fs.readFile(filepath, 'utf8', function(err, contents) {
+        console.log(contents);
+    });
+    return res.status(200).send({
+        success: 200,
+        response: "Output done"
+    })
 });
 
 module.exports = router;
